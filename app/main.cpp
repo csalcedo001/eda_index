4c565db01a1639951a0c25320e999d3ebde33e7b
César Salcedo <cesar.salcedo@utec.edu.pe>
2020-11-07 22:01:46 -0500

    Updated main to receive input

--- app/main.cpp
+++ app/main.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
 
-#include "BTree.h"
+#include "b_tree.hpp"
+#include "char_array.hpp"
 
 using namespace std;
-
-template <int T>
-struct CharArray {
-    CharArray() = default;
-    CharArray(const string& temp) { strcpy(data, temp.c_str()); }
-    CharArray(char in[T]) { memcpy(data, in, T); }
-
-    char& operator[](unsigned int idx) { return data[idx]; }
-    char data[T];
-
-    bool operator < (const CharArray<T>& idx) const {
-        string lhd(data);
-        string rhd(idx.data);
-        return lhd < rhd;
-    }
-    bool operator == (const CharArray<T>& idx) const {
-        string lhd(data);
-        string rhd(idx.data);
-        return lhd == rhd;
-    }
-};
 
 int main() {
 
@@ -31,20 +11,31 @@ int main() {
 
     //------------ READ ---------------
     fstream file("data/data/data.dat");
+
     string line;
+
     long pos = 0;
+
     while (getline(file, line)) {
         stringstream ss(line);
         string token;
-        getline(ss, token, '|');
+
+        getline(ss, token, '\t');
+
         CharArray<45> temp(token);
+
         bt.insert(temp, pos);
         pos = file.tellg();
     }
 
     //----------- SEARCH --------------
-    CharArray<45> temp("face");
+	string query;
+
+	cin >> query;
+
+    CharArray<45> temp(query);
     Record found = bt.search(temp);
+
     cout << found.word << " : " << found.definition << endl;
 
     return 0;
